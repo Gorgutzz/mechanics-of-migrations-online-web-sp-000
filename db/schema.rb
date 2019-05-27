79fa1b35338297779900fe5e2291159d77f5306c
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-05-27 01:28:00 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,11 @@
 
 ActiveRecord::Schema.define(version: 0) do
 
-  create_table "artists", force: :cascade do |t|
-  t.string "name"
-  t.string "genre"
-  t.integer "age"
-  t.string "hometown"
-end
+    create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.string "genre"
+    t.integer "age"
+    t.string "hometown"
+  end
 
 end
